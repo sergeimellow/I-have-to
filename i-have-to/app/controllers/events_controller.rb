66e3ea06f832e3456abb7d87a14f154d263b0323
Sergei Mellow <sergei.mellow@gmail.com>
2015-10-18 22:57:16 -0400

    next time fix back button on event view to return back to correct calendar

--- i-have-to/app/controllers/events_controller.rb
+++ i-have-to/app/controllers/events_controller.rb
@@ -75,6 +75,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:time, :title, :description, :calendar_id)
+      params.require(:event).permit(:event_date, :title, :description, :calendar_id)
     end
 end
